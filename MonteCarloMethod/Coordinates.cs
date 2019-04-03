1b83a7a098b31dc3972b2daf43f9cffe0d55ade4
bryang93 <47666895+bryang93@users.noreply.github.com>
2019-04-02 21:43:28 -0400

    Can generate random coords Need to store in array

---
+++ MonteCarloMethod/Coordinates.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace MonteCarloMethod
+{
+   struct Coordinates
+    {
+        // Struct holds x, y
+        public double x;
+        public double y;
+        // 2-Parameter constructor that takes new set of coords
+        public Coordinates(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        // 1-Parameter constructor that takes Random object & initialize x, y
+        public Coordinates(Random random)
+        {
+            this.x = random.NextDouble();
+            this.y = random.NextDouble();
+        }
+
+        // hypotenuse function
+        public double Function(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+            double hypotenuse = Math.Pow(x, 2) + Math.Pow(y, 2);
+            return Math.Sqrt(hypotenuse);
+        }
+    }
+
+}
--- MonteCarloMethod/Program.cs
+++ MonteCarloMethod/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Drawing;
 using System.Threading.Tasks;
 
 namespace MonteCarloMethod
@@ -10,6 +11,37 @@ namespace MonteCarloMethod
     {
         static void Main(string[] args)
         {
+            Coordinates Coords = new Coordinates(0, 0);
+            Random random = new Random();
+            Coordinates xyPairs = new Coordinates(random);
+
+            Console.WriteLine("Welcome to the Monte Carlo method\n");
+            Console.WriteLine("Select number amount to pass:");
+
+            // prompts amount of iterations & assigns variable
+            int amount = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            // counter
+            int counter = 0;
+            Console.WriteLine($"Overlap Amount:{counter}");
+            // array containing random coords
+            for (int i = 1; i <= amount; i++)
+            {
+                // gives value to x & y
+                double hypotenuse = (xyPairs.Function(xyPairs.x, xyPairs.y));
+            }
+
+            // 2D array stores value
+            double[] coordPair = new double[] { hypotenuse };
+            foreach (double xy in coordPair)
+            {
+                Console.WriteLine(hypotenuse);
+            }
+
+
+
+
         }
     }
 }
